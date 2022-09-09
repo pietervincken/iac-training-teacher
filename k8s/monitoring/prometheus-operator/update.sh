d3b632e64fec719e7a2a5109270864dae6b3b909
pietervincken <4153479+pietervincken@users.noreply.github.com>
2022-09-09 16:41:59 +0200

    upgrade prometheus

--- k8s/monitoring/prometheus-operator/update.sh
+++ k8s/monitoring/prometheus-operator/update.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-export VERSION=v0.48.1
+BUILD_IMAGES=false
 
-mkdir -p kustomize/resources || true
-mkdir -p kustomize/resources/crds || true
+### Helper
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    jq '.tag_name' --raw-output
+}
+
+VERSION=$(get_latest_release "prometheus-operator/prometheus-operator")
+
+echo $VERSION
 
 ### Update CRDs
-git clone --depth=1 https://github.com/prometheus-operator/prometheus-operator
+git clone --depth 1 --branch $VERSION https://github.com/prometheus-operator/prometheus-operator 2> /dev/null
 cp prometheus-operator/example/prometheus-operator-crd/* kustomize/resources/crds/
 cp prometheus-operator/example/rbac/prometheus-operator/prometheus-operator-deployment.yaml kustomize/resources/
 cp prometheus-operator/example/rbac/prometheus-operator/prometheus-operator-service.yaml kustomize/resources/
@@ -16,4 +23,4 @@ cp prometheus-operator/example/rbac/prometheus-operator/prometheus-operator-clus
 cp prometheus-operator/example/rbac/prometheus-operator/prometheus-operator-cluster-role-binding.yaml kustomize/resources/
 cp prometheus-operator/example/rbac/prometheus-operator-crd/prometheus-operator-crd-cluster-roles.yaml kustomize/resources/
 
-rm -rf prometheus-operator
+rm -rf prometheus-operator *.bak
