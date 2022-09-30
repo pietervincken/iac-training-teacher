a7e440e0431e7e03ac9d07cec1c596b3682a5ea0
pietervincken <4153479+pietervincken@users.noreply.github.com>
2022-09-30 10:22:55 +0200

    fix: ability to remove resource_groups with resources

--- tf/main.tf
+++ tf/main.tf
@@ -15,9 +15,12 @@ terraform {
   }
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 # Configure the Azure Active Directory Provider
